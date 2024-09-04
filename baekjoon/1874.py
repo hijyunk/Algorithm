87eca93afecfe83749acd90089d734e3d494a73e
hijyunk <ziggzagg@ajou.ac.kr>
2024-09-04 10:21:14 +0900

    1874 주석 수정

--- baekjoon/1874.py
+++ baekjoon/1874.py
@@ -7,12 +7,12 @@ answer = []
 current = 1 # 1부터 시작
 
 for num in seq:
-    while current <= num: # 현재 스택의 최상단 숫자가 목표 수열의 숫자보다 작다면
+    while current <= num: # 목표 숫자(num)에 도달할 때까지
         stack.append(current) # push
         answer.append('+')
         current += 1 # 다음 숫자
 
-    if stack[-1] == num: # 스택의 최상단 숫자가 현재 목표 수열의 숫자라면
+    if stack[-1] == num: # 스택의 최상단 숫자가 목표 숫자와 같으면
         stack.pop() # pop
         answer.append('-')
     else:
