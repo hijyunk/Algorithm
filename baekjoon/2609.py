82e5cacdf0866da2bd651b73077d2256e2a8452a
hijyunk <ziggzagg@ajou.ac.kr>
2024-10-04 04:34:26 +0900

    2609

---
+++ baekjoon/2609.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+
+# 최대공약수
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a%b
+    return a
+
+# 최대공배수
+def lcm(a, b):
+    return (a*b) // gcd(a,b)
+
+print(gcd(a,b))
+print(lcm(a,b))
