7fcbdb1e5cbf4b50aa552ebd56b137cf838a0658
hijyunk <ziggzagg@ajou.ac.kr>
2024-10-04 23:25:46 +0900

    11050 factorial

--- baekjoon/11050.py
+++ baekjoon/11050.py
@@ -1,12 +1,4 @@
+import math
+
 N, K = map(int, input().split())
-
-a = list(range(N-K+1, N+1))
-b = list(range(2, K+1))
-
-def multiply(list):
-    result = 1
-    for num in list:
-        result *= num
-    return result
-
-print(int(multiply(a)/multiply(b)))
+print(math.factorial(N) // (math.factorial(K) * math.factorial(N-K)))
