37ba11e3a207ccdc73671a51d41c5bd780d9dce0
hijyunk <ziggzagg@ajou.ac.kr>
2024-10-14 23:28:47 +0900

    11727 DP

--- baekjoon/11727.py
+++ baekjoon/11727.py
@@ -1,15 +1,16 @@
 # 2xn 직사각형
 n = int(input())
 
-dp = [0] * (n+1)
-
 if n == 1:
     print(1)
 elif n == 2:
     print(3)
 else:
+    dp = [0] * (n+1)
     dp[1] = 1
     dp[2] = 3
+
     for i in range(3, n+1):
         dp[i] = dp[i-2]*2 + dp[i-1]
+        
     print(dp[-1] % 10007)
