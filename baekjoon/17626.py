a997d82356e1d50655664ccad7d662dc61869166
hijyunk <ziggzagg@ajou.ac.kr>
2024-10-16 14:53:25 +0900

    17626

---
+++ baekjoon/17626.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+def square(n):
+    # 제곱수 1개
+    if (n**0.5)**2 == n:
+        return 1
+    
+    # 제곱수 2개
+    for i in range(1, int(n**0.5)+1):
+        # n-i^2를 제곱근하고 제곱 = n-i^2이면 n은 제곱수 2개로 이루어져있다.
+        if int((n - i**2)**0.5)**2 == (n - i**2):
+            return 2
+    
+    # 제곱수 3개
+    while n % 4 == 0:    # 4의 배수 제외
+        n //= 4
+    if n % 8 == 7:    # 8로 나눴을 때 나머지가 7이면 반드시 4개의 제곱수
+        return 4
+    
+    return 3
+
+print(square(n))
